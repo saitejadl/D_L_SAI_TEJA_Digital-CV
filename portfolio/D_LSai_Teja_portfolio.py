610b428f0bba20a422be62173dee52e92fed046a
saitejadl <125654512+saitejadl@users.noreply.github.com>
2023-05-06 16:30:01 +0530

    Update D_LSai_Teja_portfolio.py

--- portfolio/D_LSai_Teja_portfolio.py
+++ portfolio/D_LSai_Teja_portfolio.py
@@ -24,14 +24,9 @@ SOCIAL_MEDIA = {
     "Kaggle: https://www.kaggle.com/saitejadharmada": "https://www.kaggle.com/saitejadharmada"
 }
 PROJECTS = {
-    """🏆 Design and Fabrication of AI-Based Electric Vehicles Designing and Fabricating an Electric Vehicle (AIEV) that will drive semi-autonomously with the minimum human interface.                               
-                               Vehicle structure design for efficient load carry. 
-                               Maintaining load balance by positioning mechanical, electrical, and electronic components.
-                               Installed a Raspberry Pi 4 Model B, a relay circuit, and ultrasonic sensors for detecting obstacles and navigating passengers in a safe direction. """:'.',
+    """🏆 Design and Fabrication of AI-Based Electric Vehicles Designing and Fabricating an Electric Vehicle (AIEV) that will drive semi-autonomously with the minimum human interface. Vehicle structure design for efficient load carry. Maintaining load balance by positioning mechanical, electrical, and electronic components. Installed a Raspberry Pi 4 Model B, a relay circuit, and ultrasonic sensors for detecting obstacles and navigating passengers in a safe direction. """:'.',
  
-    """🏆 Personal Voice Assistant: a virtual assistant that can do various things such as tell the time, play music on YouTube, send messages on WhatsApp, search the web, and open applications on your computer, among others.
-                                It uses various APIs such as SpeechRecognition, Pyttsx3, PyWhatKit, Wikipedia, and OpenAI.
-                                It will prompt you for a password. If the password is correct, it will start listening for commands. You can ask it to perform various tasks, and it will respond accordingly. """:'.'
+    """🏆 Personal Voice Assistant: a virtual assistant that can do various things such as tell the time, play music on YouTube, send messages on WhatsApp, search the web, and open applications on your computer, among others. It uses various APIs such as SpeechRecognition, Pyttsx3, PyWhatKit, Wikipedia, and OpenAI. It will prompt you for a password. If the password is correct, it will start listening for commands. You can ask it to perform various tasks, and it will respond accordingly. """:'.'
 }
 
 
