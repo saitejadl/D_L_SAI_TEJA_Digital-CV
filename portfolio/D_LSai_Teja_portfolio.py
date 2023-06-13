d5a6e5bc5a09e084ed4e059ac5dc0aa1c6fe3fa0
saitejadl <125654512+saitejadl@users.noreply.github.com>
2023-06-13 19:25:38 +0530

    Update D_LSai_Teja_portfolio.py

--- portfolio/D_LSai_Teja_portfolio.py
+++ portfolio/D_LSai_Teja_portfolio.py
@@ -30,7 +30,7 @@ PROJECTS = {
 }
 
 
-st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON,layout="center")
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON,layout="centered")
 
 
 # --- LOAD CSS, PDF & PROFIL PIC ---
