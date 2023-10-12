7881ff37fda19a455a547bef9453e8c87590190c
LAKSHMI SAI TEJA DHARMADA <125654512+saitejadl@users.noreply.github.com>
2023-10-12 09:38:48 +0530

    Update D_LSai_Teja_portfolio.py

--- portfolio/D_LSai_Teja_portfolio.py
+++ portfolio/D_LSai_Teja_portfolio.py
@@ -41,7 +41,7 @@ else:
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "D L Sai Teja Resume.pdf"
+resume_file = current_dir / "assets" / "dlsaiteja cognizant resume.pdf"
 profile_pic = current_dir / "assets" / "Teja_profile.png"
 
 
