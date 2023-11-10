03d845fe92fc6af519fb33dd2eb852d0cec2f6da
LAKSHMI SAI TEJA DHARMADA <125654512+saitejadl@users.noreply.github.com>
2023-11-10 21:33:03 +0530

    Update D_LSai_Teja_portfolio.py

--- portfolio/D_LSai_Teja_portfolio.py
+++ portfolio/D_LSai_Teja_portfolio.py
@@ -88,7 +88,7 @@ with col2:
 st.write('\n')
 cols = st.columns([3,2,3])#len(SOCIAL_MEDIA))
 for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
-    cols[index].write(f"[{platform}]({link})")
+    cols[index].write(f"- [{platform}]({link})")
 
 anim1,anim2,anim3 = st.columns(3)
 with anim1:
@@ -107,9 +107,9 @@ st.write('\n')
 st.subheader("Qualifications")
 st.write(
     """
-- \t\t✔️ XII: Sri Chaitanya Juniour Collage \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tCGPA: 9.1, YOP: 2018
-- \t\t✔️ UG: Bonam Venkata Chalamayya Institution of Technology and Science Amalapuram - JNTUK\t\t\t\t\tCGPA: 7.04, YOP: 2022.
-- \t\t✔️ PGP: Post Graduation Program in Computational Datascience (upGrad INSOFE)\t\t\t\t\t\t\t\tDuration: May 2022–April 2023
+\t\t✔️ XII: Sri Chaitanya Juniour Collage \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tCGPA: 9.1, YOP: 2018
+\t\t✔️ UG: Bonam Venkata Chalamayya Institution of Technology and Science Amalapuram - JNTUK\t\t\t\t\tCGPA: 7.04, YOP: 2022.
+\t\t✔️ PGP: Post Graduation Program in Computational Datascience (upGrad INSOFE)\t\t\t\t\t\t\t\tDuration: May 2022–April 2023
 """
 )
 
