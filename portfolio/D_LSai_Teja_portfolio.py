a82a93cb476b16cda2b386f139c2f5d41ae4820d
LAKSHMI SAI TEJA DHARMADA <125654512+saitejadl@users.noreply.github.com>
2023-12-22 12:12:47 +0530

    Update D_LSai_Teja_portfolio.py

--- portfolio/D_LSai_Teja_portfolio.py
+++ portfolio/D_LSai_Teja_portfolio.py
@@ -201,5 +201,5 @@ st.write("""- Badminton 🏸
             - Gardening 🌱""")
 
 if st.text_input("Enter code")=="teja":
-    st.markdown("**BLUE[Hello]**":"www.google.co.in")
+    st.markdown("**BLUE[Hello]**:Hello":"www.google.co.in")
 # https://docs.google.com/presentation/d/1jrDigbMfp_wxzW7Eyu1jHRl0gkBYqv1c/edit?usp=share_link&ouid=110531620878687336760&rtpof=true&sd=true
